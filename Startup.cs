852007e64211a15e5a9af9d698ec47dcee63cc1c
Abhinav Mishra <abhinav.mishra@globallogic.com>
2020-08-22 12:46:33 +0530

    API Name Changes

--- Startup.cs
+++ Startup.cs
@@ -61,7 +61,7 @@ namespace SLB_Clock
             // specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Clock API");
                 
             });
             app.UseHttpsRedirection();
